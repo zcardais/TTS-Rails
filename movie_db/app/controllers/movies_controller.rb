0ac77f41517b154d28ac2c185fb625b86118d3c7
HorridAura <nanda1123@gmail.com>
2015-05-16 16:06:29 +0000

    Added poster to movie table but cannot display image.

--- movie_db/app/controllers/movies_controller.rb
+++ movie_db/app/controllers/movies_controller.rb
@@ -69,6 +69,6 @@ class MoviesController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def movie_params
-      params.require(:movie).permit(:name, :description, :tmeter, :genre_id, :actor_id)
+      params.require(:movie).permit(:name, :description, :tmeter, :genre_id, :actor_id, :poster_id)
     end
 end
--- movie_db/db/schema.rb
+++ movie_db/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150514150542) do
+ActiveRecord::Schema.define(version: 20150516154347) do
 
   create_table "actors", force: true do |t|
     t.string   "name"
@@ -35,6 +35,7 @@ ActiveRecord::Schema.define(version: 20150514150542) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "tmeter"
+    t.string   "poster"
   end
 
 end
